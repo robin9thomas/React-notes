dc35278fb67cbaa5b6d687cd65f6c193af4bc1ed
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-02-08 14:56:56 +0100

    Define global style with styled-components

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,22 +1,10 @@
-import styled from "styled-components";
-
-const Side = styled.aside`
-  position: fixed;
-  width: 240px;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  background-color: #1d2327;
-`;
-
-const Main = styled.main`
-  height: 100vh;
-  margin-inline-start: 240px;
-`;
+import { Side, Main } from "./App.styled";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Side>Barre latérale</Side>
       <Main>Contenu principal</Main>
     </>
---
+++ web-app/src/App.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Side = styled.aside`
+  position: fixed;
+  width: 240px;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  background-color: #1d2327;
+`;
+export const Main = styled.main`
+  height: 100vh;
+  margin-inline-start: 240px;
+`;
